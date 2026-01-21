9faed7bbad8485ca0199490f2af0c7b764a850a5
DSAlgoKhushboo <khushboo.goyal06@gmail.com>
2026-01-21 12:17:01 -0500

    initial commit for dsAlgo Project

---
+++ src/main/java/DSAlgo/DSAlgo/App.java
@@ -0,0 +1,13 @@
+package DSAlgo.DSAlgo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
