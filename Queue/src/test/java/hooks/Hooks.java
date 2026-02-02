f09ab0e1ffd96670ebed768f4dfbd60b98557120
speechari <shreya231995@gmail.com>
2026-02-02 14:37:12 -0800

    Initial Code for Queue module

---
+++ Queue/src/test/java/hooks/Hooks.java
@@ -0,0 +1,18 @@
+package hooks;
+
+import driverfactory.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        DriverFactory.getDriver();
+    }
+
+    @After
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
---
+++ Queue/src/test/java/stepdefinitions/QueueSteps.java
@@ -0,0 +1,156 @@
+@Given("User opens DS Algo Home page")
+public void user_opens_ds_algo_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on the {string} link")
+public void user_clicks_on_the_link(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User enters valid username and password")
+public void user_enters_valid_username_and_password() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on the {string} button")
+public void user_clicks_on_the_button(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("User should be redirected to the Home page")
+public void user_should_be_redirected_to_the_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("User is on the Home page")
+public void user_is_on_the_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on the Get Started button present in Queue card")
+public void user_clicks_on_the_get_started_button_present_in_queue_card() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Queue page header should be {string}")
+public void queue_page_header_should_be(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("User is in Queue Page")
+public void user_is_in_queue_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User views the Queue topics")
+public void user_views_the_queue_topics() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Topic covered should have {string}")
+public void topic_covered_should_have(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks {string}")
+public void user_clicks(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Title should be {string}")
+public void title_should_be(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Practice question should be present")
+public void practice_question_should_be_present() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Try here should be present")
+public void try_here_should_be_present() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("User is in {string} page")
+public void user_is_in_page(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks practice question")
+public void user_clicks_practice_question() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("user should navigate to practice page")
+public void user_should_navigate_to_practice_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks Try here")
+public void user_clicks_try_here() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("User should be redirected to Assessment page")
+public void user_should_be_redirected_to_assessment_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Run Button Appears on the Page")
+public void run_button_appears_on_the_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("User is in assessment page")
+public void user_is_in_assessment_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks run button after entering valid code")
+public void user_clicks_run_button_after_entering_valid_code() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Expected output should be displayed")
+public void expected_output_should_be_displayed() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks run button after entering invalid code")
+public void user_clicks_run_button_after_entering_invalid_code() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("Alert should appear with error message")
+public void alert_should_appear_with_error_message() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
