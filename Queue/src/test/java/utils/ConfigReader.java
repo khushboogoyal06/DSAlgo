8fad873c464e2c9c0dd5c34c7f6ce283f732cf62
speechari <shreya231995@gmail.com>
2026-02-15 10:46:14 -0800

    updating Queue page

--- Queue/src/test/java/utils/ConfigReader.java
+++ Queue/src/test/java/utils/ConfigReader.java
@@ -1,7 +1,42 @@
-/**
- * 
- */
-/**
- * 
- */
 package utils;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    static Properties prop;
+
+    public static Properties initProperties() {
+
+        prop = new Properties();
+
+        try {
+
+            InputStream input = ConfigReader.class
+                    .getClassLoader()
+                    .getResourceAsStream("config/config.properties");
+
+            if (input == null) {
+                System.out.println("config.properties file NOT found");
+                return prop;   
+            }
+
+            prop.load(input);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return prop;   
+    }
+
+    public static String getProperty(String key) {
+
+        if (prop == null) {
+            initProperties();
+        }
+
+        return prop.getProperty(key);
+    }
+}
