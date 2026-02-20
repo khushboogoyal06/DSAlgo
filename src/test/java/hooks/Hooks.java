f24d5d4c8093a1d542aa9129d1e1bc0916b0b0fc
speechari <shreya231995@gmail.com>
2026-02-20 13:31:00 -0800

    modified with conditional hooks

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,23 +1,43 @@
 package hooks;
 
 import io.cucumber.java.Before;
-import driverfactory.DriverFactory;
 import io.cucumber.java.After;
+
+import driverfactory.DriverFactory;
+import pages.QueuePage;
 import utils.ConfigReader;
 
 public class Hooks {
 
-    @Before
+    @Before(order=0)
     public void setUp() {
+
+        // load config file
+        ConfigReader.initProperties();
+
+        // start browser
         DriverFactory.initDriver();
+
+        // open url
         DriverFactory.getDriver().get(
-            ConfigReader.getProperty("url")
+                ConfigReader.getProperty("url")
         );
     }
+@Before(order=1, value="@login")
+   public void login() {
+	   // LOGIN
+       QueuePage queuePage =
+               new QueuePage(DriverFactory.getDriver());
 
+       queuePage.clickSignIn();
+       queuePage.login(
+               ConfigReader.getProperty("username"),
+               ConfigReader.getProperty("password")
+       );
+	   
+   }
     @After
     public void tearDown() {
-        DriverFactory.getDriver().quit();
+        DriverFactory.quitDriver();
     }
 }
-
