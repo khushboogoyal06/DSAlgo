f7598062d88ba135e848a8806ea45fbea9d6d4af
speechari <shreya231995@gmail.com>
2026-02-23 14:45:35 -0800

    updation of driver factory

--- src/test/java/driverfactory/DriverFactory.java
+++ src/test/java/driverfactory/DriverFactory.java
@@ -1,35 +1,50 @@
 package driverfactory;
 
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import utils.ConfigReader;
 
 public class DriverFactory {
 
-    private static ThreadLocal<WebDriver> mydriver = new ThreadLocal<>();
+    // single driver instance
+    public static WebDriver driver;
 
+    // initialize driver
     public static WebDriver initDriver() {
 
         String browser = ConfigReader.getProperty("browser");
 
         if (browser.equalsIgnoreCase("chrome")) {
-            mydriver.set(new ChromeDriver());
+            driver = new ChromeDriver();
         } 
         else if (browser.equalsIgnoreCase("firefox")) {
-            mydriver.set(new FirefoxDriver());
+            driver = new FirefoxDriver();
+        } 
+        else {
+            System.out.println("Browser not supported. Launching Chrome by default.");
+            driver = new ChromeDriver();
         }
 
-        getDriver().manage().window().maximize();
-        //getDriver().deleteAllCookies();
+        // basic setup
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
 
-        return getDriver();
+        return driver;
     }
 
+    // get driver
     public static WebDriver getDriver() {
-        return mydriver.get();
+        return driver;
+    }
+
+    // quit driver
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
-
-
